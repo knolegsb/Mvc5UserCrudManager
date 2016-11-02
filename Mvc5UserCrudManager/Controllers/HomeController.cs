0c7bbb31347e7f6a06237757da3dca2e9aff0b52
Sean <seon2you@hotmail.com>
2016-11-01 21:51:37 -0700

    crud

--- Mvc5UserCrudManager/Controllers/HomeController.cs
+++ Mvc5UserCrudManager/Controllers/HomeController.cs
@@ -61,52 +61,7 @@ namespace Mvc5UserCrudManager.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Create(UserViewModel model)
-        //{
-        //    User user = new Models.User
-        //    {
-        //        Id = Guid.NewGuid().ToString(),
-        //        FirstName = model.FirstName,
-        //        LastName = model.LastName,
-        //        Email = model.Email,
-        //        Gender = model.Gender
-        //    };
-        //    db.Users.Add(user);
-
-        //    var countryId = db.Countries.Where(m => m.Name == model.Country).Select(m => m.Id).FirstOrDefault();
-        //    UserCountry userCountry = new UserCountry
-        //    {
-        //        UserCountryId = Guid.NewGuid().ToString(),
-        //        UserId = user.Id,
-        //        CountryId = countryId
-        //    };
-        //    db.UserCountries.Add(userCountry);
-
-        //    UserDescription userDescription = new UserDescription
-        //    {
-        //        UserId = user.Id,
-        //        Description = model.Description
-        //    };
-        //    db.UserDescription.Add(userDescription);
-
-        //    foreach(var course in model.Courses)
-        //    {
-        //        if (course.Checked == true)
-        //        {
-        //            UserCourse userCourse = new UserCourse
-        //            {
-        //                UserCourseId = Guid.NewGuid().ToString(),
-        //                UserId = user.Id,
-        //                CourseId = user.Id,
-        //                Checked = true
-        //            };
-        //            db.UserCourses.Add(userCourse);
-        //        }                
-        //    }
-        //    await db.SaveChangesAsync();
-        //    return RedirectToAction("Index", "Home");
-        //}
+        [ValidateAntiForgeryToken]        
         public async Task<ActionResult> Create(UserViewModel model)
         {
             User user = new User
@@ -139,7 +94,6 @@ namespace Mvc5UserCrudManager.Controllers
 
             foreach (var course in model.Courses)
             {
-
                 if (course.Checked == true)
                 {
                     UserCourse userCourse = new UserCourse
@@ -156,6 +110,119 @@ namespace Mvc5UserCrudManager.Controllers
             await db.SaveChangesAsync();
             return RedirectToAction("Index", "Home");
 
+        }
+
+        public string GetUserProperty(string id, System.Linq.Expressions.Expression<Func<User, string>> selector)
+        {
+            return db.Users.Where(x => x.Id == id).Select(selector).FirstOrDefault();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Edit(string id, UserViewModel model)
+        {
+            model.FirstName = GetUserProperty(id, x => x.FirstName);
+            model.LastName = GetUserProperty(id, x => x.LastName);
+            model.Email = GetUserProperty(id, x => x.Email);
+            model.Gender = GetUserProperty(id, x => x.Gender);
+            model.Description = db.UserDescription.Where(x => x.UserId == id).Select(x => x.Description).FirstOrDefault();
+            var countryId = db.UserCountries.Where(x => x.UserId == id).Select(x => x.CountryId).FirstOrDefault();
+            model.Country = db.Countries.Where(x => x.Id == countryId).Select(x => x.Name).FirstOrDefault();
+            model.UserId = id;
+
+            CreateCourseList(model);
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserViewModel model)
+        {
+            User user = db.Users.Where(u => u.Id == model.UserId).FirstOrDefault();
+            TryUpdateModel(user, "", new string[] { "FirstName", "LastName", "Email", "Gender" });
+
+            UserDescription userDescription = db.UserDescription.Where(u => u.UserId == model.UserId).FirstOrDefault();
+            TryUpdateModel(userDescription, "", new string[] { "Description" });
+
+            UserCountry userCountry = db.UserCountries.Where(u => u.UserId == model.UserId).FirstOrDefault();
+            var countryId = db.Countries.Where(n => n.Name == model.Country).Select(i => i.Id).FirstOrDefault();
+            if (userCountry.CountryId != countryId)
+            {
+                if (userCountry != null)
+                {
+                    db.UserCountries.Remove(userCountry);
+                    db.SaveChanges();
+                }
+                UserCountry uCountry = new UserCountry
+                {
+                    UserCountryId = Guid.NewGuid().ToString(),
+                    UserId = user.Id,
+                    CountryId = countryId
+                };
+                db.UserCountries.Add(uCountry);
+            }
+
+            var userCourses = db.UserCourses.Where(x => x.UserId == model.UserId).ToList();
+            List<string> uCourseIds = new List<string>();
+            foreach (var crId in userCourses)
+            {
+                uCourseIds.Add(crId.CourseId);
+            }
+
+            var newCourses = model.Courses.Where(x => x.Checked == true).ToList();
+            List<string> nCourseIds = new List<string>();
+            foreach(var crId in newCourses)
+            {
+                nCourseIds.Add(crId.Id);
+            }
+            if (!uCourseIds.SequenceEqual(nCourseIds))
+            {
+                foreach (var crId in userCourses)
+                {
+                    UserCourse userCourse = db.UserCourses.Where(x => x.UserId == model.UserId && x.CourseId == crId.CourseId).FirstOrDefault();
+                    db.UserCourses.Remove(userCourse);
+                    db.SaveChanges();
+                }
+                foreach (var course in model.Courses)
+                {
+                    if (course.Checked == true)
+                    {
+                        UserCourse userCourse = new UserCourse
+                        {
+                            UserCourseId = Guid.NewGuid().ToString(),
+                            UserId = user.Id,
+                            CourseId = course.Id,
+                            Checked = true
+                        };
+                        db.UserCourses.Add(userCourse);
+                    }
+                }
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Delete(string id)
+        {
+            var userCourses = db.UserCourses.Where(x => x.UserId == id).ToList();
+            foreach(var crs in userCourses)
+            {
+                db.UserCourses.Remove(crs);
+            }
+
+            var userCountry = db.UserCountries.Where(x => x.UserId == id).FirstOrDefault();
+            db.UserCountries.Remove(userCountry);
+
+            var userDescription = db.UserDescription.Where(x => x.UserId == id).FirstOrDefault();
+            db.UserDescription.Remove(userDescription);
+
+            var user = db.Users.Where(x => x.Id == id).FirstOrDefault();
+            db.Users.Remove(user);
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
 
         public void CreateCourseList(UserViewModel model)
